c6e8620f34da93b5ea7dc8944f20c1ef1cebc5e0
G184192021 <218419@st.takushoku-u.ac.jp>
2022-05-26 15:34:07 +0900

    第５回課題

--- kazuate.js
+++ kazuate.js
@@ -7,28 +7,44 @@ let kaisu = 0;
 
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
+
 hantei();
-hantei();
-hantei();
-hantei();
+let judgement=document.querySelector('#print');
+judgement.addEventListener('click',hantei)
 
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() {
-    let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
+function hantei(event) {
+    let predict=document.querySelector('input[name="predict"]');
+    let yoso = predict.value;       // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
 kaisu+=1;
-console.log(kaisu+'回目の予想:'+yoso);
+//console.log(kaisu+'回目の予想:'+yoso);
+let not=document.querySelector('span#time');
+let nots=document.createElement('p');
+nots.textContent=kaisu+'回目の予想: '+yoso;
+not.insertAdjacentElement('afterbegin',nots);
+let reply=document.querySelector('p#result');
 if(kaisu>=4){
-    console.log('答えは'+kotae+'でした．すでにゲームは終わっています');
+    let p=document.createElement('p');
+    p.textContent='答えは'+kotae+'でした．すでにゲームは終わっています';
+    reply.insertAdjacentElement('afterbegin',p);
 }else if(kotae===yoso){
-    console.log('正解です．おめでとう！')
+    p=document.createElement('p');
+    p.textContent='正解です．おめでとう！';
+    reply.insertAdjacentElement('afterbegin',p);
 }else if(kaisu===3){
-    console.log('まちがい．残念でした答えは'+kotae+'です．');
+    p=document.createElement('p');
+    p.textContent='まちがい．残念でした答えは'+kotae+'です．';
+    reply.insertAdjacentElement('afterbegin',p);
 }else if(kotae>yoso){
-    console.log('まちがい．答えはもっと大きいですよ');
+    p=document.createElement('p');
+    p.textContent='まちがい．答えはもっと大きいですよ';
+    reply.insertAdjacentElement('afterbegin',p);
 }else{
-    console.log('まちがい．答えはもっと小さいですよ');
+    p=document.createElement('p');
+    p.textContent='まちがい．答えはもっと小さいですよ';
+    reply.insertAdjacentElement('afterbegin',p);
 }
 }
