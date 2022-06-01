03f579634bc5e91242007b29139ff76ee0f3aae2
G184192021 <218419@st.takushoku-u.ac.jp>
2022-06-02 05:50:07 +0900

    5th task

--- kazuate.js
+++ kazuate.js
@@ -8,14 +8,14 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
 
-hantei();
+//hantei();
 let judgement=document.querySelector('#print');
 judgement.addEventListener('click',hantei)
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei(event) {
     let predict=document.querySelector('input[name="predict"]');
-    let yoso = predict.value;       // 第5回課題:テキストボックスの数値をここに代入
+    let yoso=Math.floor(predict.value);       // 第5回課題:テキストボックスの数値をここに代入
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
